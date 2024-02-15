fd22e8003551344bae8ea3979901a022b2af6071
ArunaChoudhary11 <choudharyaruna11@gmail.com>
2024-02-15 12:38:40 +0530

    Update

---
+++ Gleam/Assets/Script/Player/PhotonShield.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PhotonShield : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Shield(Vector2 hitPoint , Vector2 hitNormal)
+    {
+        
+    }
+}
